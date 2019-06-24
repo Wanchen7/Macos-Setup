b9061890791bb79748deb33329b41a0ab3bfe896
fuwanchen <fuwanchen7@icloud.com>
2019-06-24 14:53:26 +0800

    init script

---
+++ macos-setup.py
@@ -0,0 +1,11 @@
+#!/Library/Frameworks/Python.framework/Versions/2.7/bin/python
+# -*- coding: utf-8 -*-
+
+import getpass
+import os
+import json
+import urllib2
+import subprocess
+
+name = getpass.getuser()
+print name
